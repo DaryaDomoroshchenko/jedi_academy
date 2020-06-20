e5bedcfd9d56db36d05587bdffd3b8e2d30649e0
Darya Domoroshchenko <deydra.v@gmail.com>
2020-06-20 16:05:14 +0400

    base styles & adaptive added

---
+++ index.js
@@ -0,0 +1,9 @@
+/* $(() => {
+
+  $('.intro__button').on('click', function(e){
+    $('html,body').stop().animate({ scrollTop: $('#some_point').offset().top }, 1000);
+    e.preventDefault();
+  });
+
+});
+ */
